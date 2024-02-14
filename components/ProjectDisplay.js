4d33f2301d02aa514fbd8d43cfa66f7b69599553
Colin Grob <colin.grob87@gmail.com>
2024-02-13 20:34:20 -0800

    changed project hover behavior

--- components/ProjectDisplay.js
+++ components/ProjectDisplay.js
@@ -20,7 +20,7 @@ const Project = ({ project }) => {
       </div>
       <div className="absolute bottom-3 left-3 flex flex-row space-x-4">
         {project.links.website && (
-          <div className="w-6 h-6 group-hover:scale-105 hover:cursor-pointer">
+          <div className="w-6 h-6 group-hover:scale-125 duration-200 hover:cursor-pointer">
             <a href={project.links.website} target="_blank">
               <img src="./images/world-wide-web.png" />
             </a>
